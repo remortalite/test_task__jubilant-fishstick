3dd06ca264c766a586ba8d420eb4dec1dff60ad4
Rem M. <morrig407@gmail.com>
2025-01-13 18:06:30 +0700

    Add flask and sqlalchemy to project, create main.py with basic configs

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from standart!")
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+db.init_app(app)
 
 
 if __name__ == "__main__":
-    main()
+    app.run()
