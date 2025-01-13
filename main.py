f8f45227f7e2b4d6f9aefe838d04fbe7c2bc6967
Rem M. <morrig407@gmail.com>
2025-01-13 18:36:08 +0700

    Add model Transaction

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, validates
+
+
+STATUSES = ["Waiting", "Confirmed", "Cancelled", "Expired"]
 
 
 class Base(DeclarativeBase):
@@ -20,6 +23,18 @@ class User(db.Model):
     commission_rate: Mapped[float]
     url_webhook: Mapped[str]
     
+
+class Transaction(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    sum: Mapped[float]
+    status: Mapped[str]
+
+    @validates("status")
+    def validate_status(self, key, status):
+        if status not in STATUSES:
+            raise ValueError("Failed status name validation")
+        return address
+
 
 with app.app_context():
     db.create_all()
