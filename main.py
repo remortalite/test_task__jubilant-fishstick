1f92bc4f16aa7acb166e0d6f58462e89d55ab740
Rem M. <morrig407@gmail.com>
2025-01-13 18:12:13 +0700

    Add User model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
@@ -12,6 +12,17 @@ db = SQLAlchemy(model_class=Base)
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 db.init_app(app)
+
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    balance: Mapped[float]
+    commission_rate: Mapped[float]
+    url_webhook: Mapped[str]
+    
+
+with app.app_context():
+    db.create_all()
 
 
 if __name__ == "__main__":
