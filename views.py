015917e192b14633774c16fa4aa89b61f4fc6af8
Rem M. <morrig407@gmail.com>
2025-01-20 21:53:00 +0700

    Add some stats to dashboard

--- views.py
+++ views.py
@@ -1,12 +1,20 @@
 from flask_admin import BaseView, expose
 import flask_login
 
+from models import Clients, Transactions
+
 
 class DashboardView(BaseView):
 	@flask_login.login_required
 	@expose('/')
 	def index(self):
-		return self.render('dashboard_view.html')
+		clients_count = Clients.query.count()
+		transactions_count = Transactions.query.count()
+		
+		return self.render('dashboard_view.html', 
+						   clients_count=clients_count,
+						   transactions_count=transactions_count,
+						   )
 
 	def is_accessible(self):
 		return flask_login.current_user.is_authenticated
